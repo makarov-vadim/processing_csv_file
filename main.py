463987097e71c618cc4661eb6181ce1217e939ad
Vadim Makarov <makarov.vadim.evph@gmail.com>
2025-07-14 04:59:41 +0400

    2025-07-14_1

--- main.py
+++ main.py
@@ -2,23 +2,39 @@ import argparse
 import csv
 import statistics
 from argparse import Namespace
+from typing import Callable
 
 from tabulate import tabulate
 
 
 class ProcessingCsvFile:
 
-    def __init__(self, file_name: str = None, delimiter: str = ",", quotechar: str = '"'):
+    def __init__(
+            self,
+            file_name: str|None = None,
+            argument: tuple[str, str]|None = None,
+            delimiter: str = ",",
+            quotechar: str = '"'):
+
         self._delimiter = delimiter
         self._quotechar = quotechar
-        self._file_name = file_name
 
-        self.args = None
-        self._file_content = None
+        self._parser = argparse.ArgumentParser()
+        self._args = self._parse_args()
 
-        self._parse_args()
-        self._open_file()
+        if file_name is not None:
+            self._change_arg(("file", file_name))
+        if argument is not None:
+            self._change_arg(argument)
+        self._file_content = self._read_file()
 
+    @property
+    def options(self) -> dict[str, Callable]:
+        return {
+            "where": self._where,
+            "aggregate": self._aggregate,
+            "order_by": self._order_by
+        }
 
     @property
     def delimiter(self):
@@ -34,9 +50,9 @@ class ProcessingCsvFile:
     def file_content(self):
         return self._file_content
 
-    # @property
-    # def args(self):
-    #     return self.args
+    @property
+    def args(self) -> Namespace:
+        return self._args
 
 
     @property
@@ -62,50 +78,58 @@ class ProcessingCsvFile:
         print(tabulate(tabular_data, headers="keys", tablefmt="psql"))
 
 
-    def _open_file(self):
-        if self.file_name is None:
-            if self.args.file is None:
-                print('Не указан путь к файлу. Пример: "--file file_path"')
-                exit()
-            else:
-                self._file_name = self.args.file
+    def _read_file(self):
+        if self.args.file is None:
+            print('Не указан путь к файлу. Пример: "--file file_path"')
+            exit()
+        else:
+            self._file_name = self.args.file
         try:
             with open(self.file_name, encoding='utf-8') as file:
                 rows = csv.DictReader(file, delimiter=self.delimiter, quotechar=self.quotechar)
-                self._file_content = [{key: self.convert_to_numeric(value) for key, value in row.items()} for row in rows]
+                return [{key: self.convert_to_numeric(value) for key, value in row.items()} for row in rows]
         except FileNotFoundError:
             print('Введен некорректный путь к файлу. Пример: "--file file_path"')
             exit()
 
 
     def _parse_args(self):
-        self._parser = argparse.ArgumentParser()
-
         self._parser.add_argument("--file", type=str, help='Путь к файлу. Пример: "--file file_path')
         self._parser.add_argument("--where", type=str, help='Условие фильтрации. Пример: --where "column_name=value"')
         self._parser.add_argument("--aggregate", type=str, help='Способ агрегации. Пример: --aggregate "column_name=max"')
         self._parser.add_argument("--order-by", type=str, help='Способ сортировки. Пример: --order-by "column_name=desc"')
 
-        self.args = self._parser.parse_args()
+        return self._parser.parse_args([])
+
+
+    def _change_arg(self, arg: tuple[str, str]):
+        option, value = arg
+        if hasattr(self.args, option):
+            setattr(self.args, option, value)
+        else:
+            print(f"Аргумент {arg} задан неверно")
 
 
     def print_file_content(self):
         self._print_table(self.file_content)
+        return self.file_content
 
 
-    def filter_equal(self, column: str, value: str | int | float):
+    def _filter_equal(self, column: str, value: str | int | float):
         try:
             result = [row for row in self.file_content if row[column] == value]
             self._print_table(result)
+            return result
         except KeyError:
             print('Указан некорректный столбец для аргумента --where')
             exit()
 
 
-    def filter_less_than(self, column: str, value: str | int | float):
+    def _filter_less_than(self, column: str, value: str | int | float):
         try:
             result = [row for row in self.file_content if row[column] < value]
             self._print_table(result)
+            return result
         except KeyError:
             print(f'Указан некорректный столбец для аргумента --where: "{column}"')
             exit()
@@ -115,10 +139,11 @@ class ProcessingCsvFile:
 
 
 
-    def filter_greater_than(self, column: str, value: str | int | float):
+    def _filter_greater_than(self, column: str, value: str | int | float):
         try:
             result = [row for row in self.file_content if row[column] > value]
             self._print_table(result)
+            return result
         except KeyError:
             print(f'Указан некорректный столбец для аргумента --where: "{column}"')
             exit()
@@ -126,138 +151,131 @@ class ProcessingCsvFile:
             print(f'Указано некорректное значение для аргумента --where: "{value}"')
             exit()
 
-    def aggregate_min(self, column: str):
+    def _aggregate_min(self, column: str):
         try:
             min_row = min(self.file_content, key=lambda row: row[column])
             result = [
                 {"min": min_row[column]}
             ]
             self._print_table(result)
+            return result
         except KeyError:
             print(f'Указан некорректный столбец для аргумента --aggregate: "{column}"')
             exit()
 
 
-    def aggregate_max(self, column: str):
+    def _aggregate_max(self, column: str):
         try:
             max_row = max(self.file_content, key=lambda row: row[column])
             result = [
                 {"max": max_row[column]}
             ]
             self._print_table(result)
+            return result
         except KeyError:
             print(f'Указан некорректный столбец для аргумента --aggregate: "{column}"')
             exit()
 
 
-    def aggregate_avg(self, column: str):
+    def _aggregate_avg(self, column: str):
         try:
             result = [
                 {"avg": statistics.mean([row[column] for row in self.file_content])}
             ]
             self._print_table(result)
+            return result
         except KeyError:
             print(f'Указан некорректный столбец для аргумента --aggregate: "{column}"')
             exit()
 
 
-    def order_by_asc(self, column: str):
+    def _order_by_asc(self, column: str):
         try:
             result = sorted(self.file_content, key=lambda row: row[column], reverse=False)
             self._print_table(result)
+            return result
         except KeyError:
             print(f'Указан некорректный столбец для аргумента --order-by: "{column}"')
             exit()
 
 
-    def order_by_desc(self, column: str):
+    def _order_by_desc(self, column: str):
         try:
             result = sorted(self.file_content, key=lambda row: row[column], reverse=True)
             self._print_table(result)
+            return result
         except KeyError:
             print(f'Указан некорректный столбец для аргумента --order-by: "{column}"')
             exit()
 
+    def _where(self):
+        _options: dict[str, Callable] = {
+            "<": self._filter_less_than,
+            ">": self._filter_greater_than,
+            "=": self._filter_equal
+        }
+        _err_message = 'Некорректное условие в аргументе --where. Пример: --where "column_name=value"'
 
-def where(processing_csv_file: ProcessingCsvFile):
-    _functions = {
-        "<": processing_csv_file.filter_less_than,
-        ">": processing_csv_file.filter_greater_than,
-        "=": processing_csv_file.filter_equal
-    }
-    _err_message = 'Некорректное условие в аргументе --where. Пример: --where "column_name=value"'
-
-    try:
-        for operator in _functions:
-            if operator in processing_csv_file.args.where:
-                column, value = processing_csv_file.args.where.split(operator)
-                value = processing_csv_file.convert_to_numeric(value)
-                _functions[operator](column, value)
-                break
-        else:
+        try:
+            for operator in _options:
+                if operator in self.args.where:
+                    column, value = self.args.where.split(operator)
+                    value = self.convert_to_numeric(value)
+                    return _options[operator](column, value)
+            else:
+                print(_err_message)
+        except ValueError:
             print(_err_message)
-    except ValueError:
-        print(_err_message)
 
 
-def aggregate(processing_csv_file: ProcessingCsvFile):
-    _functions = {
-        "min": processing_csv_file.aggregate_min,
-        "max": processing_csv_file.aggregate_max,
-        "avg": processing_csv_file.aggregate_avg
-    }
-    _err_message = 'Некорректное условие в аргументе --aggregate. Пример: --aggregate "column_name=max"'
+    def _aggregate(self):
+        _options: dict[str, Callable] = {
+            "min": self._aggregate_min,
+            "max": self._aggregate_max,
+            "avg": self._aggregate_avg
+        }
+        _err_message = 'Некорректное условие в аргументе --aggregate. Пример: --aggregate "column_name=max"'
 
-    try:
-        column, method = processing_csv_file.args.aggregate.split("=")
-        _functions[method](column)
-    except ValueError:
-        print(_err_message)
-    except KeyError:
-        print('Некорректный способ агрегации. Используйте "min", "max" или "avg"')
-
-
-def order_by(processing_csv_file: ProcessingCsvFile):
-    _functions = {
-        "asc": processing_csv_file.order_by_asc,
-        "desc": processing_csv_file.order_by_desc
-    }
-    _err_message = 'Некорректное условие в аргументе --order-by. Пример: --order-by "column_name=desc"'
-
-    try:
-        column, method = processing_csv_file.args.order_by.split("=")
-        _functions[method](column)
-    except ValueError:
-        print(_err_message)
-    except KeyError:
-        print('Некорректный способ сортировки. Используйте "asc" или "desc"')
+        try:
+            column, method = self.args.aggregate.split("=")
+            return _options[method](column)
+        except ValueError:
+            print(_err_message)
+        except KeyError:
+            print('Некорректный способ агрегации. Используйте "min", "max" или "avg"')
 
 
-def main(file_name: str = None, option: str = None, argument: str = None):
-    _options = {
-        "where": where,
-        "aggregate": aggregate,
-        "order_by": order_by
-    }
+    def _order_by(self):
+        _options: dict[str, Callable] = {
+            "asc": self._order_by_asc,
+            "desc": self._order_by_desc
+        }
+        _err_message = 'Некорректное условие в аргументе --order-by. Пример: --order-by "column_name=desc"'
 
-    processing_csv_file = ProcessingCsvFile(file_name)
+        try:
+            column, method = self.args.order_by.split("=")
+            return _options[method](column)
+        except ValueError:
+            print(_err_message)
+        except KeyError:
+            print('Некорректный способ сортировки. Используйте "asc" или "desc"')
 
-    if file_name:
-        if all((option, argument)) and option in _options:
-            processing_csv_file.args = argparse.Namespace(**{"file": file_name, option: argument})
-        processing_csv_file.args = argparse.Namespace(**{"file": file_name})
+    def process_csv_file(self):
+        args_without_file = {k: v for k, v in vars(self.args).items() if k != "file"}
 
-    if processing_csv_file.args.file is None:
-        print("Не указан путь к файлу")
+        if all(map(lambda i: args_without_file[i] is None, args_without_file.keys())):
+            return  self.print_file_content()
+        else:
+            for option, value in args_without_file.items():
+                if value is not None:
+                    return self.options[option]()
+            return None
 
-    for csv_option in _options:
-        if hasattr(processing_csv_file.args, csv_option):
-            _options[csv_option](processing_csv_file)
-            break
-    else:
-        processing_csv_file.print_file_content()
+
+def main(file_name: str = None, argument: tuple[str, str] | None = None):
+    processing_csv_file = ProcessingCsvFile(file_name, argument)
+    return processing_csv_file.process_csv_file()
 
 
 if __name__ == "__main__":
-    # main(file_name="products.csv", option="where", argument="rating>4.5")
-    main(file_name="products.csv")
+    main()
--- test_main.py
+++ test_main.py
@@ -1,6 +1,106 @@
+from argparse import Namespace
+
 import pytest
 from main import main
 
+class CorrectData:
+    WITHOUT_ARGUMENTS = [
+        {"name": "iphone 15 pro", "brand": "apple", "price": 999, "rating": 4.9},
+        {"name": "galaxy s23 ultra", "brand": "samsung", "price": 1199, "rating": 4.8},
+        {"name": "redmi note 12", "brand": "xiaomi", "price": 199, "rating": 4.6},
+        {"name": "iphone 14", "brand": "apple", "price": 799, "rating": 4.7},
+        {"name": "galaxy a54", "brand": "samsung", "price": 349, "rating": 4.2},
+        {"name": "poco x5 pro", "brand": "xiaomi", "price": 299, "rating": 4.4},
+        {"name": "iphone se", "brand": "apple", "price": 429, "rating": 4.1},
+        {"name": "galaxy z flip 5", "brand": "samsung", "price": 999, "rating": 4.6},
+        {"name": "redmi 10c", "brand": "xiaomi", "price": 149, "rating": 4.1},
+        {"name": "iphone 13 mini", "brand": "apple", "price": 599, "rating": 4.5}
+    ]
+
+
+    WHERE_PRICE_LESS_400 = [
+        {"name": "redmi note 12", "brand": "xiaomi", "price": 199, "rating": 4.6},
+        {"name": "galaxy a54", "brand": "samsung", "price": 349, "rating": 4.2},
+        {"name": "poco x5 pro", "brand": "xiaomi", "price": 299, "rating": 4.4},
+        {"name": "redmi 10c", "brand": "xiaomi", "price": 149, "rating": 4.1}
+    ]
+
+    WHERE_PRICE_GREATER_400 = [
+        {"name": "iphone 15 pro", "brand": "apple", "price": 999, "rating": 4.9},
+        {"name": "galaxy s23 ultra", "brand": "samsung", "price": 1199, "rating": 4.8},
+        {"name": "iphone 14", "brand": "apple", "price": 799, "rating": 4.7},
+        {"name": "iphone se", "brand": "apple", "price": 429, "rating": 4.1},
+        {"name": "galaxy z flip 5", "brand": "samsung", "price": 999, "rating": 4.6},
+        {"name": "iphone 13 mini", "brand": "apple", "price": 599, "rating": 4.5}
+    ]
+
+    WHERE_PRICE_EQUAL_999 = [
+        {"name": "iphone 15 pro", "brand": "apple", "price": 999, "rating": 4.9},
+        {"name": "galaxy z flip 5", "brand": "samsung", "price": 999, "rating": 4.6}
+    ]
+
+    AGGREGATE_RATING_MIN = [{"min": 4.1}]
+
+    AGGREGATE_RATING_MAX = [{"max": 4.9}]
+
+    AGGREGATE_RATING_AVG = [{"avg": 4.49}]
+
+    ORDER_BY_BRAND_ASC = [
+        {"name": "iphone 15 pro", "brand": "apple", "price": 999, "rating": 4.9},
+        {"name": "iphone 14", "brand": "apple", "price": 799, "rating": 4.7},
+        {"name": "iphone se", "brand": "apple", "price": 429, "rating": 4.1},
+        {"name": "iphone 13 mini", "brand": "apple", "price": 599, "rating": 4.5},
+        {"name": "galaxy s23 ultra", "brand": "samsung", "price": 1199, "rating": 4.8},
+        {"name": "galaxy a54", "brand": "samsung", "price": 349, "rating": 4.2},
+        {"name": "galaxy z flip 5", "brand": "samsung", "price": 999, "rating": 4.6},
+        {"name": "redmi note 12", "brand": "xiaomi", "price": 199, "rating": 4.6},
+        {"name": "poco x5 pro", "brand": "xiaomi", "price": 299, "rating": 4.4},
+        {"name": "redmi 10c", "brand": "xiaomi", "price": 149, "rating": 4.1}
+    ]
+
+    ORDER_BY_BRAND_DESC = [
+        {"name": "redmi note 12", "brand": "xiaomi", "price": 199, "rating": 4.6},
+        {"name": "poco x5 pro", "brand": "xiaomi", "price": 299, "rating": 4.4},
+        {"name": "redmi 10c", "brand": "xiaomi", "price": 149, "rating": 4.1},
+        {"name": "galaxy s23 ultra", "brand": "samsung", "price": 1199, "rating": 4.8},
+        {"name": "galaxy a54", "brand": "samsung", "price": 349, "rating": 4.2},
+        {"name": "galaxy z flip 5", "brand": "samsung", "price": 999, "rating": 4.6},
+        {"name": "iphone 15 pro", "brand": "apple", "price": 999, "rating": 4.9},
+        {"name": "iphone 14", "brand": "apple", "price": 799, "rating": 4.7},
+        {"name": "iphone se", "brand": "apple", "price": 429, "rating": 4.1},
+        {"name": "iphone 13 mini", "brand": "apple", "price": 599, "rating": 4.5}
+    ]
+
+file_name = "products.csv"
+valid_data = [
+    {"description": "without arguments", "argument": None,
+     "correct_result": CorrectData.WITHOUT_ARGUMENTS},
+    {"description": "where price<400", "argument": ("where", "price<400"),
+     "correct_result": CorrectData.WHERE_PRICE_LESS_400},
+    {"description": "where price>400", "argument": ("where", "price>400"),
+     "correct_result": CorrectData.WHERE_PRICE_GREATER_400},
+    {"description": "where price=999", "argument": ("where", "price=999"),
+     "correct_result": CorrectData.WHERE_PRICE_EQUAL_999},
+    {"description": "aggregate rating=min", "argument": ("aggregate", "rating=min"),
+     "correct_result": CorrectData.AGGREGATE_RATING_MIN},
+    {"description": "aggregate rating=max", "argument": ("aggregate", "rating=max"),
+     "correct_result": CorrectData.AGGREGATE_RATING_MAX},
+    {"description": "aggregate rating=avg", "argument": ("aggregate", "rating=avg"),
+     "correct_result": CorrectData.AGGREGATE_RATING_AVG},
+    {"description": "order_by brand=asc", "argument": ("order_by", "brand=asc"),
+     "correct_result": CorrectData.ORDER_BY_BRAND_ASC},
+    {"description": "order_by brand=desc", "argument": ("order_by", "brand=desc"),
+     "correct_result": CorrectData.ORDER_BY_BRAND_DESC}
+]
+
+
 class TestProcessingCsvFile:
-    def test_file(self):
-        pass
+
+    @pytest.mark.parametrize("data", valid_data)
+    def test_valid_data(self, data):
+        actual_result = main(file_name=file_name, argument=data["argument"])
+        correct_result = data["correct_result"]
+
+        err_text = f"В тесте {data["description"]} \nфактический результат: \n{actual_result} \nне совпадает с ожидаемым: \n{correct_result}"
+
+        assert actual_result == correct_result, err_text
